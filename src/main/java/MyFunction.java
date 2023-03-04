25c8a08ffe1a37f25bd0a004fcd91f8878e8cc4a
Роман Казаченко <312515@niuitmo.ru>
2023-03-04 15:26:17 +0300

    func implementation

---
+++ src/main/java/MyFunction.java
@@ -0,0 +1,21 @@
+public class MyFunction {
+
+  private final Trigonometry tr;
+  private final MyLog log;
+
+  public MyFunction() {
+    tr = new Trigonometry();
+    log = new MyLog();
+  }
+
+  public double calculate(double x, double eps) {
+    if (x <= 0) {
+      return ((((Math.pow(tr.cos(x, eps) / tr.tan(x, eps), 3) * tr.sin(x, eps)) / tr.sec(x, eps))
+          * ((tr.cos(x, eps) - (tr.tan(x, eps) * tr.sec(x, eps))) / (tr.sec(x, eps) + (
+          tr.cos(x, eps) * tr.cot(x, eps))))));
+    } else {
+      return (((((log.log(x, 10) - log.log(x, 2)) * log.log(x, 2)) + (log.log(x, 10) / log.ln(x)))
+          + log.log(x, 10)) + ((log.log(x, 10) / log.log(x, 2)) / log.log(x, 3)));
+    }
+  }
+}
