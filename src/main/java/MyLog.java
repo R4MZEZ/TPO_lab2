586f736c0729175e735dabc3f3aebd422d669f66
Mike Steph <timetocook420@gmail.com>
2023-03-04 14:46:52 +0300

    Log Function

---
+++ src/main/java/MyLog.java
@@ -0,0 +1,18 @@
+public class MyLog {
+
+    private final static MyLn ln = new MyLn();
+
+    private Double exp = 0.0001;
+
+    public void setExp(double x) {
+        this.exp = x;
+    }
+
+    public double ln(double x) {
+        return ln.calculate(x, exp);
+    }
+
+    public double log(double arg, double base) {
+        return ln.calculate(arg)/ln.calculate(base);
+    }
+}
--- src/test/java/LogTest.java
+++ src/test/java/LogTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class MyLnTest {
+public class LogTest {
     @ParameterizedTest
     @ValueSource(doubles = {1, 2, 3, 5, 10, 0.1})
     public void generalTest(double x) {
-        MyLn l = new MyLn();
-        System.out.println(l.calculate(x));
+        MyLog l = new MyLog();
+        System.out.println(l.log(x, 2));
     }
 }
