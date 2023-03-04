b3439fd6c5f60e0be1b3f64f2c90cdafb31e70a5
Роман Казаченко <312515@niuitmo.ru>
2023-03-04 14:23:41 +0300

    sin

---
+++ src/main/java/MySin.java
@@ -0,0 +1,37 @@
+public class MySin {
+
+  private final static double DELTA = 0.0001;
+
+  public double calculate(double x) {
+    return calculate(x, DELTA);
+  }
+
+  public double calculate(double x, double eps) {
+    if (x == Double.NEGATIVE_INFINITY || x == Double.POSITIVE_INFINITY || Double.isNaN(x)) {
+      return Double.NaN;
+    } else if (x > 0) {
+      while (x > Math.PI) {
+        x -= 2 * Math.PI;
+      }
+    }else if (x < 0){
+      while (x < -Math.PI) {
+        x += 2 * Math.PI;
+      }
+    }
+
+    double res = x, term = x;
+    double divisible = x * x * x;
+    int divider = 6, sign = -1, pow = 3;
+
+    while (Math.abs(term) > eps) {
+      term = sign * (divisible / divider);
+      res += term;
+
+      sign *= -1;
+      divisible *= x * x;
+      divider = divider * ++pow * ++pow;
+    }
+
+    return res;
+  }
+}
